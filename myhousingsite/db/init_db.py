cc006077ae77492e43a5cb2f5db68a883764f8d4
Myapi314 <97209406+Myapi314@users.noreply.github.com>
2023-06-15 14:44:26 -0600

    Moved into class to avoid creating multiples of db

--- myhousingsite/db/init_db.py
+++ myhousingsite/db/init_db.py
@@ -13,66 +13,77 @@ class HousingDatabase():
         # Connection to db, will either connect to existing or create new
         self.conn = sqlite3.connect(self.db_path)
         self.conn.row_factory = sqlite3.Row
+
+        # Create cursor
         self.curs = self.conn.cursor()
         return self.conn
+    
+    def create_db(self):
+        try: 
+            self.connect_to_db()
 
-db = HousingDatabase(DB_NAME)
-conn = db.connect_to_db()
+            # Create residents info table
+            self.curs.execute("""CREATE TABLE IF NOT EXISTS tb_resident_info (
+                        resident_id INTEGER PRIMARY KEY, 
+                        first_name TEXT NOT NULL,
+                        last_name TEXT NOT NULL,
+                        email TEXT NOT NULL UNIQUE,
+                        phone TEXT NOT NULL UNIQUE,
+                        birthdate TEXT NOT NULL,
+                        gender TEXT NOT NULL
+                        )
+            """)
 
-# Create cursor
-curs = conn.cursor()
+            # Create housing complexes table
+            # type: student or community
+            self.curs.execute("""CREATE TABLE IF NOT EXISTS tb_housing_complexes (
+                        complex_name TEXT NOT NULL UNIQUE, 
+                        street_address TEXT NOT NULL,
+                        type TEXT NOT NULL
+                        )
+            """)
 
-# Create residents info table
-curs.execute("""CREATE TABLE IF NOT EXISTS tb_resident_info (
-            resident_id INTEGER PRIMARY KEY, 
-            first_name TEXT NOT NULL,
-            last_name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE,
-            phone TEXT NOT NULL UNIQUE,
-            birthdate TEXT NOT NULL,
-            gender TEXT NOT NULL
-            )
-""")
+            # Create student housing table
+            # type: private or shared
+            self.curs.execute("""CREATE TABLE IF NOT EXISTS tb_student_housing_units (
+                        complex_id INTEGER NOT NULL,
+                        unit_number TEXT NOT NULL,
+                        gender TEXT NOT NULL,
+                        room TEXT NOT NULL,
+                        bed TEXT NOT NULL,
+                        room_type TEXT NOT NULL,
+                        rent_price REAL NOT NULL,
+                        FOREIGN KEY (complex_id) REFERENCES tb_housing_complexes (rowid),
+                        UNIQUE(unit_number, room, bed)
+                        )
+            """)
 
-# Create housing complexes table
-# type: student or community
-curs.execute("""CREATE TABLE IF NOT EXISTS tb_housing_complexes (
-            complex_name TEXT NOT NULL UNIQUE, 
-            street_address TEXT NOT NULL,
-            type TEXT NOT NULL
-            )
-""")
+            # Create lease table
+            self.curs.execute("""CREATE TABLE IF NOT EXISTS tb_leases (
+                        resident_id INTEGER NOT NULL,
+                        unit_id INTEGER NOT NULL,
+                        lease_start TEXT NOT NULL,
+                        lease_end TEXT NOT NULL,
+                        created_date TEXT NOT NULL,
+                        FOREIGN KEY (resident_id) REFERENCES tb_resident_info (resident_id),
+                        FOREIGN KEY (unit_id) REFERENCES tb_student_housing_units (rowid),
+                        UNIQUE(resident_id, unit_id, lease_start)
+                        )
+            """)
 
-# Create student housing table
-# type: private or shared
-curs.execute("""CREATE TABLE IF NOT EXISTS tb_student_housing_units (
-            complex_id INTEGER NOT NULL,
-            unit_number TEXT NOT NULL,
-            gender TEXT NOT NULL,
-            room TEXT NOT NULL,
-            bed TEXT NOT NULL,
-            room_type TEXT NOT NULL,
-            rent_price REAL NOT NULL,
-            FOREIGN KEY (complex_id) REFERENCES tb_housing_complexes (rowid),
-            UNIQUE(unit_number, room, bed)
-            )
-""")
+            # Commits change to DB
+            self.conn.commit()
 
-# Create lease table
-curs.execute("""CREATE TABLE IF NOT EXISTS tb_leases (
-            resident_id INTEGER NOT NULL,
-            unit_id INTEGER NOT NULL,
-            lease_start TEXT NOT NULL,
-            lease_end TEXT NOT NULL,
-            created_date TEXT NOT NULL,
-            FOREIGN KEY (resident_id) REFERENCES tb_resident_info (resident_id),
-            FOREIGN KEY (unit_id) REFERENCES tb_student_housing_units (rowid),
-            UNIQUE(resident_id, unit_id, lease_start)
-            )
-""")
+        except Exception as e:
+            print("Error in initializing database: ", e)
 
-# Commits change to DB
-conn.commit()
+        finally:
+            # Close connection
+            self.conn.close()
 
-# Close connection
-conn.close()
+def main():
+    db = HousingDatabase(DB_NAME)
+    db.create_db()
+
+if __name__=='__main__':
+    main()
